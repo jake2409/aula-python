11336ace0790390352f5b82f8df78f9354324455
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-07 21:03:50 -0300

    Operadores logicos e mais um desafio logico

--- aula07033/main.py
+++ aula07033/main.py
@@ -1,13 +1,33 @@
+'''
 valor_amaciante = float(input("Qual o valor do amaciante ?"))
 valor_desconto  = float(input("Qual o valor do desconto ?"))
 
 valor_desconto = valor_desconto/100
-
 valor_descontado = valor_amaciante * valor_desconto
 
 valor_total = valor_amaciante - valor_descontado
 
-
-
-
 print(f"O valor total do amaciante é {valor_total}\nE o valor descontado é {valor_descontado}")
+
+a = 2
+b = 3
+c = a
+
+a = b
+b = c
+
+a = 2
+b = 3
+
+a ,b = b, a #somente no python isso funcionaria
+print(a,b)
+'''
+a = 1
+b = 2.0
+c = "gabriel"
+d = true
+e = false
+f = none
+
+#operadores lógicos >, >=, <, <=, ==, !=, and, or, not, in
+
